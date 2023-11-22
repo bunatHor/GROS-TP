b98c6bd666d838a835cb701caeeb1de1f558d3d5
bunatHor <chaanfac@gmail.com>
2023-11-22 15:43:36 +0100

    premier jet mdm.sh

---
+++ mdm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+for arg in $@
+do
+	if [[ $arg =~ ^remove (.+) (.+) ]]
+	then
+	 	echo "remove:\1:\2" 
+
+  	elif [[ $arg =~ ^add (.+) (.+) ]]
+        then
+
+  	elif [[ $arg =~ ^list (.+) (.+) ]]
+        then
+
+	elif [[ $arg =~ ^change (.+) (.+) ]]
+        then
+
+
+	fi
+done
+
+ssh machine@1234 'bash -s' < core.sh 
+ssh machine@1.2.3.4 core.sh remove $1 $2
