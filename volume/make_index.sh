01560228ca2df258de5dbfaff21592eecb36bf62
bunatHor <chaanfac@gmail.com>
2023-11-23 10:07:35 +0100

    make_index.sh

---
+++ volume/make_index.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#IP=1.2.3.2 	####### L'ip de notre docker nginx
+
+IFS=$'\n'
+diff_grp=$(cat bdd.csv | cut -d":" -f4 | sort -u)
+
+for i in $diff_grp
+do
+	mkdir $i
+	grep $i bdd.csv | cut -d":" -f 1,2,3 > $i/index.html
+done
+
+	
+
+
+
+
